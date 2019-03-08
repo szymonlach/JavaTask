0db61e78c95e79d44cf62d053377f62426c1c83e
Szymon Lach <szymom.sp1@gmail.com>
2019-03-08 18:07:34 +0100

    Remove 'Magical numbers'

--- 07_PaymentCalculatorForEmployee/src/pl/lach/PaymentCalculator.java
+++ 07_PaymentCalculatorForEmployee/src/pl/lach/PaymentCalculator.java
@@ -5,6 +5,8 @@ import java.math.BigDecimal;
 
 public class PaymentCalculator {
 
+    private static final BigDecimal YEARLY_ZUS = BigDecimal.valueOf(12000);
+
     public static BigDecimal nettoYearPayment(Employee employee) {
         BigDecimal yearNettoPayment = employee.getMonthlyPayment().multiply(BigDecimal.valueOf(12));
         return yearNettoPayment;
@@ -14,7 +16,6 @@ public class PaymentCalculator {
 
     public static BigDecimal bruttoYearPayment(Employee employee) {
         BigDecimal bruttoPayment = employee.getMonthlyPayment().divide(BigDecimal.valueOf(0.8)).multiply(BigDecimal.valueOf(12));
-        BigDecimal incomeTax = BigDecimal.valueOf(12000);
-        return bruttoPayment.add(incomeTax);
+        return bruttoPayment.add(YEARLY_ZUS);
     }
 }
