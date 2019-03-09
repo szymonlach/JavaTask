26f8a31f31b549510283ac5a97493d862949fd7b
Szymon Lach <szymom.sp1@gmail.com>
2019-03-09 20:23:31 +0100

    Some refractor and deleted 'Magic numbers'

--- 12_Playlist/src/pl/lach/Playlist.java
+++ 12_Playlist/src/pl/lach/Playlist.java
@@ -2,25 +2,29 @@ package pl.lach;
 
 public class Playlist {
 
+    private static final int SENCONDS_IN_HOUR =3600;
+    private static final int SENCONDS_IN_MINUTE =60;
+
     public static String playlistDuration(Song... songs) {
         StringBuilder stringBuilder = new StringBuilder();
         int summaryDurationInSeconds = 0;
         for (int i = 0; i < songs.length; i++) {
             summaryDurationInSeconds += songs[i].getDuration();
         }
-        int hours = summaryDurationInSeconds / 3600;
-        String hoursToShow;
-        if (hours < 10) hoursToShow = "0" + String.valueOf(hours);
-        else hoursToShow = String.valueOf(hours);
-        int minutes = (summaryDurationInSeconds - hours * 3600) / 60;
-        String minutesToShow;
-        if (minutes < 10) minutesToShow = "0" + String.valueOf(minutes);
-        else minutesToShow = String.valueOf(minutes);
-        int seconds = summaryDurationInSeconds - minutes * 60 - hours * 3600;
-        String secondsToShow;
-        if (seconds < 10) secondsToShow = "0" + String.valueOf(seconds);
-        else secondsToShow = String.valueOf(seconds);
+        int hours = summaryDurationInSeconds / SENCONDS_IN_HOUR;
+        String hoursToShow = timeToString(hours);
+        int minutes = (summaryDurationInSeconds - hours * SENCONDS_IN_HOUR) / SENCONDS_IN_MINUTE;
+        String minutesToShow = timeToString(minutes);
+        int seconds = summaryDurationInSeconds - minutes * SENCONDS_IN_MINUTE - hours * SENCONDS_IN_HOUR;
+        String secondsToShow = timeToString(seconds);
         return String.format("%s:%s:%s", hoursToShow, minutesToShow, secondsToShow);
+    }
 
+    private static String timeToString(int time) {
+        String result;
+        if (time < 10) result = "0" + String.valueOf(time);
+        else if (time == 0) result = "00";
+        else result = String.valueOf(time);
+        return result;
     }
 }
