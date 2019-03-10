8846b011735d733ea5689e4ada02887997d1d972
Szymon Lach <szymom.sp1@gmail.com>
2019-03-10 19:56:57 +0100

    Solution for fourteenth task is complete

--- 14_Hangman/src/pl/lach/components/Game.java
+++ 14_Hangman/src/pl/lach/components/Game.java
@@ -1,9 +1,56 @@
 package pl.lach.components;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Game {
 
-    public void startGame() {
-        System.out.println("Try guess the correct password");
-        System.out.println();
+    private Logic logic;
+    private boolean continueProgram;
+
+
+    public Game() {
+        this.logic = new Logic();
+        continueProgram = true;
     }
+
+    public void startGame() {
+        Scanner scanner = new Scanner(System.in);
+        while (continueProgram) {
+            System.out.println("Try guess the correct password");
+            System.out.println("Password\n" + logic.getTemporaryPassword());
+
+            while (!logic.isGameEnds()) {
+                System.out.println("Type a letter");
+                String input = scanner.next();
+                System.out.println("Password\n" + logic.checkPasswordContainCharacter(input));
+            }
+
+            System.out.println(logic.getGameResult());
+            System.out.println("1 - Play again");
+            System.out.println("2 - Exit");
+            while (true) {
+                try {
+                    continueProgram = isCloseProgram(scanner.nextInt());
+                    break;
+                } catch (IllegalArgumentException | InputMismatchException e) {
+                    System.err.println("Type 1 or 2");
+                }
+            }
+
+        }
+    }
+
+    private boolean isCloseProgram(int input) {
+        if (input == 1) {
+            logic.reloadGame();
+            return true;
+        } else if (input == 2) {
+            System.out.println("Goodbye");
+            return false;
+
+        } else throw new IllegalArgumentException();
+    }
+
+
 }
--- 14_Hangman/src/pl/lach/components/Logic.java
+++ 14_Hangman/src/pl/lach/components/Logic.java
@@ -1,6 +1,70 @@
 package pl.lach.components;
 
-public class Logic {
-    
+import java.util.ArrayList;
+import java.util.List;
 
+public class Logic {
+
+    private String finalPassword;
+    private StringBuilder stringBuilder;
+    private int numberOfMisses;
+    private String gameResult;
+    private List<String> typedCharacters;
+
+    public Logic() {
+        finalPassword = new PasswordsManage().takeRandomPassword().toUpperCase();
+        stringBuilder = new StringBuilder(hidePasword(finalPassword));
+        numberOfMisses = 0;
+        typedCharacters = new ArrayList<>();
+    }
+
+    public String checkPasswordContainCharacter(String character) {
+        character = character.toUpperCase();
+        if (!typedCharacters.contains(character)) {
+            if (finalPassword.contains(character)) {
+                int index = finalPassword.indexOf(character);
+                while (index >= 0) {
+                    stringBuilder.setCharAt(index, finalPassword.charAt(index));
+                    index = finalPassword.indexOf(character, index + 1);
+                }
+            } else {
+                numberOfMisses++;
+            }
+            typedCharacters.add(character);
+        }
+        return stringBuilder.toString();
+    }
+
+    public boolean isGameEnds() {
+        if (numberOfMisses >= 8) {
+            gameResult = "Sorry but you lose!!!\n" + "The correct password was: " + finalPassword;
+            return true;
+        } else if (!stringBuilder.toString().contains("*")) {
+            gameResult = "Congratulation you won!!!\n" + "Password: " + finalPassword;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void reloadGame() {
+        finalPassword = new PasswordsManage().takeRandomPassword().toUpperCase();
+        stringBuilder = new StringBuilder(hidePasword(finalPassword));
+        numberOfMisses = 0;
+        typedCharacters = new ArrayList<>();
+    }
+
+
+    private String hidePasword(String uncoveredPassword) {
+        return uncoveredPassword.replaceAll("\\w", "*");
+    }
+
+
+    public String getTemporaryPassword() {
+        return stringBuilder.toString();
+    }
+
+    public String getGameResult() {
+        return gameResult;
+    }
 }
