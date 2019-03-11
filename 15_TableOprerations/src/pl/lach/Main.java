ca4f06c4b5d90e1d0c2b68cba8bee923805e8850
Szymon Lach <szymom.sp1@gmail.com>
2019-03-11 15:00:08 +0100

    Second part is ready

--- 15_TableOprerations/src/pl/lach/Main.java
+++ 15_TableOprerations/src/pl/lach/Main.java
@@ -1,34 +1,45 @@
 package pl.lach;
 
+import javafx.scene.control.Tab;
+
 /**
  * Define the class, and in it the usable methods for operations on arrays:
- * <p>
+ *
  * 1. Takes two arrays of two-dimensional integers and returns the one in which the sum of numbers is greater,
  * 2. It accepts two arrays with subtitles and returns the one in which the total number of characters in all words is greater. For example, for arrays ["aa", "bb"], ["aaa", "bbb"] the second table will be returned, because the total number of characters is 6,
  * 3. Returns the largest number from the int [] array,
  * 4. Returns the smallest number from the int [] array,
  * 5. Adopts a two-dimensional array of int [] [] numbers and returns a one-dimensional array in which the sum of the elements is the largest (the largest row). For example, for an array [[1, 2, 3], [7, 8, 9], [3, 4, 5]], an array [7, 8, 9] should be returned.
  * We assume that nobody will pass nulls or empty arrays to methods.
- * <p>
+ *
  * In a separate class, test the operation of each method on at least one sample data.
  */
 public class Main {
     public static void main(String[] args) {
-        int[][] testTable1 = new int[2][3];
-        testTable1[0][0] = 3;
-        testTable1[0][1] = 3;
-        testTable1[0][2] = 5;
-        testTable1[1][0] = 3;
-        testTable1[1][1] = 3;
-        testTable1[1][2] = 3;
+        //Solution for 1.
+        int[][] testNumberTable1 = new int[2][3];
+        testNumberTable1[0][0] = 3;
+        testNumberTable1[0][1] = 3;
+        testNumberTable1[0][2] = 5;
+        testNumberTable1[1][0] = 3;
+        testNumberTable1[1][1] = 3;
+        testNumberTable1[1][2] = 3;
 
-        int[][] testTable2 = new int[2][2];
-        testTable2[0][0] = 5;
-        testTable2[0][1] = 5;
-        testTable2[1][0] = 5;
-        testTable2[1][1] = 1;
+        int[][] testNumberTable2 = new int[2][2];
+        testNumberTable2[0][0] = 5;
+        testNumberTable2[0][1] = 5;
+        testNumberTable2[1][0] = 5;
+        testNumberTable2[1][1] = 1;
 
-        System.out.println(TableUtils.selectTableWithGreaterSumOfElements(testTable1,testTable2).equals(testTable2));
+        System.out.println(TableUtils.selectTableWithGreaterSumOfElements(testNumberTable1,testNumberTable2).equals(testNumberTable1));
+
+        //Solution for 2.
+        String[] testStringTable1 = new String[]{"aa","bb"};
+        String[] testStringTable2 = new String[]{"aaa","bbb"};
+        System.out.println(TableUtils.selectTableWithMoreCharacters(testStringTable1,testStringTable2).equals(testStringTable2));
+
+
+
 
     }
 }
--- 15_TableOprerations/src/pl/lach/TableUtils.java
+++ 15_TableOprerations/src/pl/lach/TableUtils.java
@@ -8,6 +8,20 @@ public class TableUtils {
         } else return table2;
     }
 
+    public static String[] selectTableWithMoreCharacters(String[] table1, String[] table2) {
+        if (calculateSumOfCharacters(table1) > calculateSumOfCharacters(table2)) {
+            return table1;
+        } else return table2;
+    }
+
+    private static int calculateSumOfCharacters(String[] table) {
+        int numberOfCharacters = 0;
+        for (int i = 0; i < table.length; i++) {
+            numberOfCharacters += table[i].length();
+        }
+        return numberOfCharacters;
+    }
+
     private static int calculateSumOfElements(int[][] table) {
         int sum = 0;
 
@@ -15,7 +29,6 @@ public class TableUtils {
             for (int j = 0; j < table[i].length; j++) {
                 sum += table[i][j];
             }
-
         }
         return sum;
     }
