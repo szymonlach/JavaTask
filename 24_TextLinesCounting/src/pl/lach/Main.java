88e3a220a0d1a3c27dcf9b4f6cfe638dccbbd253
Szymon Lach <szymom.sp1@gmail.com>
2019-03-14 18:00:56 +0100

    Added content of the task in Main.class

--- 24_TextLinesCounting/src/pl/lach/Main.java
+++ 24_TextLinesCounting/src/pl/lach/Main.java
@@ -2,6 +2,11 @@ package pl.lach;
 
 import java.io.IOException;
 
+/**
+ * Create a program that counts the number of lines of text for all files in the specified folder.
+ * <p>
+ * The result should be a list of files along with information on the number of lines and the sum of all lines in the folder. The program should search the folder recursively.
+ */
 public class Main {
     public static void main(String[] args) {
         CatalogReader catalogReader = new CatalogReader();
